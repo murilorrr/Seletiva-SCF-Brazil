f44f48fa5a4049964432015b5bb103cc1cfb17a5
murilorsv14 <murilorsv14@gmail.com>
2023-06-14 01:29:57 -0300

    add a basic auth with jwt

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+const { authenticateUser } = require("./authMiddleware");
+const jwt = require('jsonwebtoken');
 
 var teste1 = require("./teste1");
 var teste2 = require("./teste2");
 var teste3 = require("./teste3");
 var teste4 = require("./teste4");
 var teste5 = require("./teste5");
+const { HttpStatus } = require("./httpStatus");
 
-
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -17,9 +19,9 @@ app.use(express.urlencoded());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
   res.send(`get user/ </br>
   get users/ </br>
   post users/ </br>
@@ -28,15 +30,27 @@ app.get('/', function(req, res){
   `);
 });
 
+app.post("/auth", (req, res, next) => {
+  const { name, job } = req.body;
+  const token = jwt.sign({ name, job }, 'chave-secreta', { expiresIn: '1h' });
+
+  res.json({ token });
+
+  if (job === "Desenvolvedor") {
+    next();
+  } else {
+    return res.status(HttpStatus.UNAUTHORIZED.code).send("Permission denied");
+  }
+});
+
 app.get("/user", teste1.getUser);
 app.get("/users", teste1.getUsers);
 app.post("/users", teste2);
-app.delete("/users", teste3);
-app.put("/users", teste4.updateUser);
+app.delete("/users", authenticateUser, teste3);
+app.put("/users", authenticateUser, teste4.updateUser);
 app.get("/users/access", teste5);
 
-
-const port  = 3000;
-app.listen(port, function(){
-  console.log('Express server listening on port ' + port);
+const port = 3000;
+app.listen(port, function () {
+  console.log("Express server listening on port " + port);
 });
---
+++ authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const { HttpStatus } = require("./httpStatus");
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(HttpStatus.UNAUTHORIZED.code).send("Unauthorized");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, "chave-secreta");
+    req.user = decodedToken.user;         
+
+    next();
+  } catch (error) {
+    return res.status(HttpStatus.UNAUTHORIZED.code).send("Unauthorized");
+  }
+};
+
+module.exports = { authenticateUser };
