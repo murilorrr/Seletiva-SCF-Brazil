88ab0f79881969f430a70ec4eebdc2d1742f0d7f
murilorsv14 <murilorsv14@gmail.com>
2023-06-14 01:10:34 -0300

    add more http status codes

--- httpStatus.js
+++ httpStatus.js
@@ -2,9 +2,9 @@ const HttpStatus = {
   OK: { code: 200 },
   CREATED: { code: 201 },
   BAD_REQUEST: { code: 400 },
-  NOT_FOUND: { code: 404 },
-  INTERNAL_SERVER_ERROR: { code: 500 },
-  // Adicione mais códigos e descrições conforme necessário
+  UNAUTHORIZED: { code: 401 },
+  NOT_FOUND: { code: 404 }, 
+  INTERNAL_SERVER_ERROR: { code: 500 }
 };
 
 module.exports = { HttpStatus };
