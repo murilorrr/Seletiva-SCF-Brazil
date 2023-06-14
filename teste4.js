ae90909baf87515add189bdd48904c58fd4a8da2
murilorsv14 <murilorsv14@gmail.com>
2023-06-13 22:28:59 -0300

    refactoring the fourth test

--- teste4.js
+++ teste4.js
@@ -1,13 +1,21 @@
-var data =  require("./fakeData");
+const fs = require("fs");
+const data = require("./fakeData");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+const updateUser = (req, res) => {
+  const { id } = req.query;
+  const { name, job } = req.body;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+  const user = data.find((item) => item.id === id);
+  if (user) {
+    user.name = name;
+    user.job = job;
 
-    res.send(reg);
+    res.status(200).send(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+};
 
+module.exports = {
+  updateUser,
 };
