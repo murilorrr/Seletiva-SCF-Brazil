10a83c94cc502d236def1ac7470e65decac7672e
murilorsv14 <murilorsv14@gmail.com>
2023-06-14 00:59:42 -0300

    refactoring with the new database version

--- teste5.js
+++ teste5.js
@@ -1,4 +1,4 @@
-const data = require("./fakeData");
+const { findUserByName } = require("./fakeData");
 const { HttpStatus } = require("./httpStatus");
 
 module.exports = function (req, res) {
@@ -6,8 +6,8 @@ module.exports = function (req, res) {
 
   if(!name) return res.status(HttpStatus.BAD_REQUEST.code).send("name is required");
 
-  const user = data.find((user) => user.name === name);
-  const userCounter = user.count ? user.count : 0;
+  const user = findUserByName(name);
+  const userCounter = user?.count ? user.count : 0;
 
   res.status(HttpStatus.OK.code).send("Usuário " + name + "  foi lido " + userCounter + " vezes.");
 };
